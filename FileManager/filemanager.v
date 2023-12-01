d677d21e48b4e6901d6e3d8f3563e36fe4f034dd
Yan Fonseca <yanfonseca2002@gmail.com>
2023-12-01 11:57:17 -0300

    Separação dos arquivos

--- FileManager/filemanager.v
+++ FileManager/filemanager.v
@@ -1,27 +1,6 @@
 module filemanager
-import os 
-
-pub struct Plane {
-	id int
-	appearance_time int
-
-	earliest_landing_time int
-	target_landing_time int
-	latest_landing_time int
-
-	penalty_for_landing_before_target f32
-	penalty_for_landing_after_target f32
-
-	separation_time []int
-}
-
-
-pub struct Problem {
-	number_of_planes int
-	freeze_time int
-
-	planes []Plane
-}
+import os
+import models
 
 pub fn read_data(path string) Problem {
 	mut counter := 0
---
+++ Models/models.v
@@ -0,0 +1,23 @@
+module models
+
+pub struct Plane {
+	id int
+	appearance_time int
+
+	earliest_landing_time int
+	target_landing_time int
+	latest_landing_time int
+
+	penalty_for_landing_before_target f32
+	penalty_for_landing_after_target f32
+
+	separation_time []int
+}
+
+
+pub struct Problem {
+	number_of_planes int
+	freeze_time int
+
+	planes []Plane
+}
