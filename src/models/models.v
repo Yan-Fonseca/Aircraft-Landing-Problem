c27145ed8b1785bdeffb3001a0e3296260a06038
Yan Fonseca <yanfonseca2002@gmail.com>
2023-12-06 18:38:10 -0300

    cálculo da viabilidade de inserção de determinado avião em uma pista

--- src/models/models.v
+++ src/models/models.v
@@ -5,6 +5,8 @@ pub struct Plane {
 	id int
 	appearance_time int
 
+	selected_time int
+
 	earliest_landing_time int
 	target_landing_time int
 	latest_landing_time int
@@ -33,14 +35,19 @@ pub:
 pub struct Solution {
 pub mut:
 	runways []Runway
+	global_cost f32
 pub:
 	number_of_runways int @[required]
 }
 
 // Métodos
 
-pub fn (u Problem) calculate_the_viability_for_landing_in_runway(last_plane_to_land_in_the_runway Plane, plane_in_air Plane) int {
-	time_distance_between_planes := plane_in_air.target_landing_time - last_plane_to_land_in_the_runway.target_landing_time
-	separation_time := last_plane_to_land_in_the_runway.separation_time[plane_in_air.id]
-	return time_distance_between_planes - separation_time
+pub fn (u Problem) calculate_the_viability_for_landing_in_runway(last_plane_in_the_runway Plane, plane_in_air Plane) int {
+	time_distance_between_planes := plane_in_air.target_landing_time - last_plane_in_the_runway.selected_time
+	separation_time := last_plane_in_the_runway.separation_time[plane_in_air.id]
+	
+	if time_distance_between_planes >= separation_time {
+		return 0
+	}
+	return time_distance_between_planes
 }
