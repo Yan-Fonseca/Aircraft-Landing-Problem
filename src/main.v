87d840da171d9c9850561f7f0a08e4cf55315bf5
Yan Fonseca <yanfonseca2002@gmail.com>
2023-12-06 20:19:20 -0300

    Adição da solução na main

--- src/main.v
+++ src/main.v
@@ -1,9 +1,11 @@
 module main
 import file_manager {read_data}
 import constructive {constructive}
+import models {Solution}
 
 fn main() {
 	problem := read_data("/home/yan/inteligencia_computacional/ALP/instances/airland1.txt")
 	
-	constructive(problem,2)
+	solution := constructive(problem,2)
+	
 }
