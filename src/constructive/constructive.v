8376c0d336237dba1f31f9d6907abb4089378b97
Yan Fonseca <yanfonseca2002@gmail.com>
2023-12-06 13:40:56 -0300

    Adicionando o primeiro avião da fila na pista 1

--- src/constructive/constructive.v
+++ src/constructive/constructive.v
@@ -5,14 +5,14 @@ import models {Plane,Solution,Problem,Runway}
 
 pub fn constructive(problem Problem, number_of_runways int) Solution {
 	mut solution := Solution{number_of_runways : number_of_runways} // Solução inicial vazia
-	//mut runways := []Runway{len : problem.number_of_planes} // vetor de pistas vazio (destinado a mostrar os aviões agendados)
+	mut runways := []Runway{len : problem.number_of_planes} // vetor de pistas vazio (destinado a mostrar os aviões agendados)
 
 	mut planes := problem.planes.clone()
 	planes.sort(a.target_landing_time > b.target_landing_time) // Ordenando a fila de aviões em voo pelo tempo alvo
 
-	for plane in planes {
-		print(plane.id.str() + " - " + plane.target_landing_time.str() +"\n")
-	}
+	runways[0].planes.prepend(planes.pop())
+
+	print(runways[0].planes[0])
 
 	return solution
 }
