0ab94aecdf42c36cef2fe90c8596a6883a692a91
Yan Fonseca <yanfonseca2002@gmail.com>
2023-12-09 16:31:29 -0300

    merge com SpiderMonkey

--- src/main.v
+++ src/main.v
@@ -3,9 +3,10 @@ import file_manager {read_data}
 import constructive {constructive, generate_initial_population}
 
 fn main() {
-	problem := read_data("/home/iamthemage/Documentos/Aircraft-Landing-Problem/instances/airland1.txt")
+	problem := read_data("/home/yan/inteligencia_computacional/ALP/instances/airland1.txt")
 	
 	mut solution := generate_initial_population(problem, 2, 5)
+	print(solution)
 	//mut solution := constructive(problem,2)
 	//solution.value_of_solution()
 	//print("Valor da solução atual: " + solution.global_cost.str() + "\n")
